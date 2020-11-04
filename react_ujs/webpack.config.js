547979667c9e66ca297184bba295f76f7b4ff1b8
Kazki Matz <kazki.matz@gmail.com>
2020-11-04 18:27:32 +0700

    Not sure why I need to do this

--- react_ujs/webpack.config.js
+++ react_ujs/webpack.config.js
@@ -8,23 +8,8 @@ module.exports = {
     libraryTarget: 'umd'
   },
   externals: {
-    'react': {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom'
-    },
-    'react-dom/server': {
-      root: 'ReactDOMServer',
-      commonjs2: 'react-dom/server',
-      commonjs: 'react-dom/server',
-      amd: 'react-dom/server'
-    }
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'react-dom/server': 'ReactDOMServer'
   }
 };
